77ddcf5f7168f1b545a350a72991dbfda7594b11
augusstt06 <augusstt06@gmail.com>
2025-01-23 22:42:56 +0900

    add: class declaration

--- lib/utils/todo_item.dart
+++ lib/utils/todo_item.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 class TodoItem extends StatelessWidget {
+  // 인스턴스가 생성되는 시점에 값을 넣어줘야 하기 때문에 final 변수 선언
   final bool isChecked;
   final String todoText;
-
+  // onChanged 함수를 인자로 받는데, bool 타입이거나 null 타입
   final Function(bool?)? onChanged;
+
   const TodoItem(
       {super.key,
       required this.isChecked,
