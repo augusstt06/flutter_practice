45ea914d72e726687e1dcd9e817498b65c9dbb74
augusstt06 <augusstt06@gmail.com>
2025-01-23 22:50:56 +0900

    add: item list

--- lib/home.dart
+++ lib/home.dart
@@ -9,25 +9,35 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List todoList = [
+    ["Todo", false],
+    ["Todo2", true],
+    ["Todo3", false],
+  ];
+
+  // 체크박스클릭 함수
+  void handlecheckBox(bool? value, int index) => setState(() {
+        todoList[index][1] = !todoList[index][1];
+      });
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepPurple[200],
-      appBar: AppBar(
-        title: Text('Sample Todo'),
-        backgroundColor: Colors.deepPurple[400],
-        // 그림자 깊이
-        elevation: 10,
-      ),
-      body: Column(
-        children: [
-          TodoItem(
-            onChanged: (value) {},
-            todoText: 'Todo',
-            isChecked: false,
-          )
-        ],
-      ),
-    );
+        backgroundColor: Colors.deepPurple[200],
+        appBar: AppBar(
+          title: Text('Sample Todo'),
+          backgroundColor: Colors.deepPurple[400],
+          // 그림자 깊이
+          elevation: 10,
+        ),
+        body: ListView.builder(
+          itemCount: todoList.length,
+          itemBuilder: (context, index) {
+            return TodoItem(
+                isChecked: todoList[index][1],
+                todoText: todoList[index][0],
+                onChanged: (value) => handlecheckBox(value, index));
+          },
+        ));
   }
 }
